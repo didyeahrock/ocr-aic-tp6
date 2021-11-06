3dbd7b775ada674d929459fd9b14482f03e18600
didyeahrock <didyeahrock@gmail.com>
2021-11-06 03:57:02 +0100

    Last Commit

--- installation-prerequis.py
+++ installation-prerequis.py
@@ -62,7 +62,7 @@ try:
     os.system("wget https://github.com/glpi-project/glpi/releases/download/9.5.6/glpi-9.5.6.tgz")
     os.system("tar -zxvf glpi-9.5.6.tgz ")
 # Moving /tmp/glpi to the root folder of apache2 
-    os.system("sudo mv glpi /var/www/html/")
+    os.system("sudo mv glpi /var/www/html/glpi")
 # make www-data owner of GLPI folder
     os.system("sudo chown -R www-data /var/www/html/glpi")
 # setting apache2 fot GLPI
