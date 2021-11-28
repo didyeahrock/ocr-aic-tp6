c6cee6b0921c28eecd91b5bc8019a3c434d5670e
didyeahrock <didyeahrock@gmail.com>
2021-11-28 18:36:15 +0100

    final 3

--- installation-prerequis.py
+++ installation-prerequis.py
@@ -9,7 +9,6 @@
     Tested with Python 3.7 running on a Ubuntu 20.04 
 
 """
-
 #Import of required modules
 
 import logging
@@ -52,14 +51,17 @@ try:
     logging.info("system update and paquet Installation...")
     os.system("sudo apt update ; apt upgrade")
     os.system("sudo dpkg --configure -a")
+    os.system("sudo apt install -y apache2 php mariadb-server curl python3 python3-pip libmariadbclient-dev pip php-mysql php-json php-gd php-curl php-mbstring php-cas php-xml php-cli php-imap php-ldap php-xmlrpc php-apcu php7.4-intl php7.4-bz2 php7.4-zip
+"""
     os.system("sudo apt install -y apache2 php mariadb-server curl python3 python3-pip libmariadbclient-dev pip")
     os.system("sudo apt install -y php-mysql php-json php-gd php-curl php-mbstring php-cas php-xml php-cli php-imap php-ldap php-xmlrpc php-apcu")
     os.system("sudo apt-get install -y php7.4-intl")
     os.system("sudo apt-get install -y php7.4-bz2")
     os.system("sudo apt-get install -y php7.4-zip")
-    os.system("sudo pip install upgrade setuptools")
-    os.system("sudo pip install validators")
-    os.system("sudo pip install mariadb")
+"""
+    os.system("sudo pip install upgrade setuptools validators mariadb")
+#    os.system("sudo pip install validators")
+#    os.system("sudo pip install mariadb")
     os.system("sudo mysql_secure_installation")
 # Installation of GLPI
     os.system("cd /tmp")
